6ee5b1a47a37d98607630af55d414eeb16a2716a
bflojo <baggycravens@gmail.com>
2017-09-14 11:25:16 -0700

    commit day 2 added locations to db

--- Model/Player.cs
+++ Model/Player.cs
@@ -9,7 +9,11 @@ namespace SnoahRpg.Model
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Health { get; set; }
-        public string Image { get; set; }
+        public int MaxHealth { get; set; }
+        public int CurrentHealth { get; set; }
+        public Item CurrentHelm { get; set; }
+        public Item CurrentChest { get; set; }
+        public Item CurrentWrist { get; set; }
+        public Item CurrentWeapon { get; set; }
     }
 }
