cd0a2e70ae7e35227b755612421b28b2dd150373
bflojo <baggycravens@gmail.com>
2017-09-13 17:00:05 -0700

    merges

--- Model/Item.cs
+++ Model/Item.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,26 +20,3 @@ namespace SnoahRpg.Model
 
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SnoahRpg.Model
-{
-    public class Item
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-
-        public string Power { get; set; }
-
-        public string Type { get; set; }
-
-        public string Image { get; set; }
-
-
-    }
-}
->>>>>>> 151848cf6b74594dce7b8ba219270174e8fabd71
